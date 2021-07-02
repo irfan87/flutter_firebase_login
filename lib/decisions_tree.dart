7422542a30eed8fe2dcac10d3b2417feb4d1a510
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2021-07-02 23:24:52 +0800

    firebase package installed

---
+++ lib/decisions_tree.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_firebase_login/login_page.dart';
+
+class DecisionsTree extends StatefulWidget {
+  const DecisionsTree({Key? key}) : super(key: key);
+
+  @override
+  _DecisionsTreeState createState() => _DecisionsTreeState();
+}
+
+class _DecisionsTreeState extends State<DecisionsTree> {
+  @override
+  Widget build(BuildContext context) {
+    return LoginPage();
+  }
+}
